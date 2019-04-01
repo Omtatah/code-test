85cc4d3ccfb32005582678fc86dd489f53e78442
Omtatah <omtatahmurtallah@gmail.com>
2019-04-01 11:03:31 +0300

    add variables for the input button answers

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,17 @@ $(document).ready(function(){
 var submit = document.getElementById("form1")
 
 $(document).ready(function(){
-  $(class="3").click(function(){
-    return submit
+  $('#3').click(function(){
+    var number1 = $(input[name=answer1]:checked).val();
+    var number2 = $(input[name=answer2]:checked).val();
+    var number3 = $(input[name=answer3]:checked).val();
+    var number4 = $(input[name=answer4]:checked).val();
+    var number5 = $(input[name=answer5]:checked).val();
+    var number6 = $(input[name=answer6]:checked).val();
+    var number7 = $(input[name=answer7]:checked).val();
+    var number8 = $(input[name=answer8]:checked).val();
+    var number9 = $(input[name=answer9]:checked).val();
+    var number10 = $(input[name=answer10]:checked).val();
   })
 })
 
