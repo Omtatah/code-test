2e0669e3b37fc0a47cafdb193caaa379dd3dd024
Omtatah <omtatahmurtallah@gmail.com>
2019-04-01 12:43:52 +0300

    add JS if statements to the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  $("img").hover(function(){
+$(document).ready(function () {
+  $("img").hover(function () {
     alert("This is an image")
   });
 });
@@ -7,26 +7,62 @@ $(document).ready(function(){
 
 var submit = document.getElementById("form1")
 
-$(document).ready(function(){
-  $('#3').click(function(){
-    var number1 = $(input[name=answer1]:checked).val();
-    var number2 = $(input[name=answer2]:checked).val();
-    var number3 = $(input[name=answer3]:checked).val();
-    var number4 = $(input[name=answer4]:checked).val();
-    var number5 = $(input[name=answer5]:checked).val();
-    var number6 = $(input[name=answer6]:checked).val();
-    var number7 = $(input[name=answer7]:checked).val();
-    var number8 = $(input[name=answer8]:checked).val();
-    var number9 = $(input[name=answer9]:checked).val();
-    var number10 = $(input[name=answer10]:checked).val();
+$(document).ready(function () {
+  $('#3').click(function () {
+    var number1 = $(input[name = answer1]: checked).val();
+    var number2 = $(input[name = answer2]: checked).val();
+    var number3 = $(input[name = answer3]: checked).val();
+    var number4 = $(input[name = answer4]: checked).val();
+    var number5 = $(input[name = answer5]: checked).val();
+    var number6 = $(input[name = answer6]: checked).val();
+    var number7 = $(input[name = answer7]: checked).val();
+    var number8 = $(input[name = answer8]: checked).val();
+    var number9 = $(input[name = answer9]: checked).val();
+    var number10 = $(input[name = answer10]: checked).val();
 
-    var numbers = [number1, number2, number3, number4, number5, number6, number7, number8, number9, number10]
+    var numbers = [number1, number2, number3, number4, number5, number6, number7, number8, number9, number10];
+    var right = 0;
+    var wrong = 0;
+
+      if (number1 === input.correct) {
+        right++
+      } 
+      if (number2 === input.correct) {
+        right++
+      } 
+      if (number3 === input.correct) {
+        right++
+      } 
+      if (number4 === input.correct) {
+        right++
+      } 
+      if (number5 === input.correct) {
+        right++
+      } 
+      if (number6 === input.correct) {
+        right++
+      } 
+      if (number7 === input.correct) {
+        right++
+      } 
+      if (number8 === input.correct) {
+        right++
+      } 
+      if (number9 === input.correct) {
+        right++
+      } 
+      if (number10 === input.correct) {
+        right++
+      } 
     
-    var right = 0
-
-    if(numbers == .correct ) {
-      
+    
+    var percentage = right/10*100
+  
+    if (percentage >= 79){
+      remark
     }
+
+  }
   })
 })
 
